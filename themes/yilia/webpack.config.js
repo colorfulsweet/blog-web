a56e371bb0d6424304b6f54bb11c33b018ab6dc6
结发受长生 <init_pro@aliyun.com>
2019-05-17 15:51:06 +0800

    修改webpack配置

--- themes/yilia/webpack.config.js
+++ themes/yilia/webpack.config.js
@@ -10,97 +10,100 @@ const minifyHTML = {
   minifyJS:true
 }
 
-module.exports = {
-  entry: {
-    main: "./source-src/js/main.js",
-    slider: "./source-src/js/slider.js",
-    mobile: ["babel-polyfill", "./source-src/js/mobile.js"],
-    viewer: "./source-src/js/viewer.js",
-    waifu: "./source-src/js/waifu.js"
-  },
-  output: {
-    publicPath: '/',
-    path: __dirname+"/source",
-    filename: "js/[name].[chunkhash:8].js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: {
-          loader: 'babel-loader', 
-          options:{cacheDirectory: true}
+module.exports = function(env, argv) {
+  // 是否是生产环境
+  let isProd = argv.mode === 'production'
+  return {
+    entry: {
+      main: './source-src/js/main.js',
+      slider: './source-src/js/slider.js',
+      mobile: ['babel-polyfill', './source-src/js/mobile.js'],
+      viewer: './source-src/js/viewer.js',
+      waifu: './source-src/js/waifu.js'
+    },
+    output: {
+      publicPath: '/',
+      path: __dirname+'/source',
+      filename: isProd ? 'js/[name].[chunkhash].js' : 'js/[name].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: {
+            loader: 'babel-loader', 
+            options:{cacheDirectory: true}
+          },
+          exclude: /node_modules/
+        },{
+          test: /\.(scss|sass)$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'postcss-loader',
+            'sass-loader'
+          ]
+        },{
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'postcss-loader'
+          ]
+        },{
+          test: /\.(png|jpe?g|gif|ico)$/,
+          use: {
+            loader: 'url-loader',
+            options: {
+              limit: 1000,
+              publicPath: '../',
+              name: 'images/[name].[ext]'
+            }
+          }
+        },{
+          test: /\.(svg|eot|ttf|woff2?|otf)$/,
+          use: {
+            loader: 'url-loader',
+            options: {
+              limit: 1000,
+              publicPath: '../',
+              name: 'fonts/[name].[hash:6].[ext]'
+            }
+          }
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: isProd ? 'css/[name].[contenthash].css' : 'css/[name].css',
+        chunkFilename: '[id].css'
+      }),
+      new OptimizeCssAssetsPlugin({
+        assetNameRegExp: /viewer\..*?\.css$/g,
+        cssProcessorPluginOptions: {
+          preset: ['default', { discardComments: { removeAll: true } }],
         },
-        exclude: /node_modules/
-      },{
-        test: /\.(scss|sass)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader"
-        ]
-      },{
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader"
-        ]
-      },{
-        test: /\.(png|jpe?g|gif|ico)$/,
-        use: {
-          loader: "url-loader",
-          options: {
-            limit: 1000,
-            publicPath: "../",
-            name: "images/[name].[ext]"
-          }
-        }
-        
-      },{
-        test: /\.(svg|eot|ttf|woff2?|otf)$/,
-        use: {
-          loader: "url-loader",
-          options: {
-            limit: 1000,
-            publicPath: "../",
-            name: "fonts/[name].[hash:6].[ext]"
-          }
-        }
-      }
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "css/[name].[contenthash:8].css",
-      chunkFilename: "[id].css"
-    }),
-    new OptimizeCssAssetsPlugin({
-      assetNameRegExp: /viewer\..*?\.css$/g,
-      cssProcessorPluginOptions: {
-        preset: ['default', { discardComments: { removeAll: true } }],
-      },
-      canPrint: true
-    }),
-    new HtmlWebpackPlugin({
-      inject: false,
-      cache: false,
-      minify: minifyHTML,
-      template: './source-src/template/script.html',
-      filename: '../layout/_partial/script.ejs'
-    }),
-    new HtmlWebpackPlugin({
-      inject: false,
-      cache: false,
-      minify: minifyHTML,
-      template: './source-src/template/css.html',
-      filename: '../layout/_partial/css.ejs'
-    }),
-    new CleanPlugin(['source/js/*.js','source/css/*.css','source/fonts/*'],{
-      verbose: true,
-      dry: false,
-    })
-  ],
-  // watch: true
+        canPrint: true
+      }),
+      new HtmlWebpackPlugin({
+        inject: false,
+        cache: false,
+        minify: minifyHTML,
+        template: './source-src/template/script.html',
+        filename: '../layout/_partial/script.ejs'
+      }),
+      new HtmlWebpackPlugin({
+        inject: false,
+        cache: false,
+        minify: minifyHTML,
+        template: './source-src/template/css.html',
+        filename: '../layout/_partial/css.ejs'
+      }),
+      new CleanPlugin(['source/js/*.js','source/css/*.css','source/fonts/*'],{
+        verbose: true,
+        dry: false,
+      })
+    ],
+    watch: !isProd
+  }
 }
