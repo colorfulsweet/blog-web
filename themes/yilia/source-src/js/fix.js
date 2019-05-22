9fe33ed376ec78303875c00b3229ef345fa06eac
结发受长生 <init_pro@aliyun.com>
2019-05-22 23:56:22 +0800

    解决top按钮生成多个问题

--- themes/yilia/source-src/js/fix.js
+++ themes/yilia/source-src/js/fix.js
@@ -18,7 +18,9 @@ function init() {
   
   // 避免由于动画带来的fix元素定位失效, 移到动画元素外层
   var sideOpt = document.querySelector('.wrap-side-operation')
-  document.getElementById('container').appendChild(sideOpt)
+  if(sideOpt) {
+    document.getElementById('container').appendChild(sideOpt)
+  }
 }
 
 export default { init }
